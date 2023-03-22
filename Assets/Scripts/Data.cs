33988804362788f4482e48f5d31d8942bed5bd7b
Demet Yayla <dmtyayla@gmail.com>
2023-03-22 21:07:05 +0300

    data capture synchronized, ohne image data

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -5,25 +5,18 @@ using UnityEngine;
 
 public class Data : MonoBehaviour
 {
-    private static int saveRate = 100;
-
     public int gameObjectCode = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         gameObjectCode = DataCollector.addNewObject(this.gameObject);
-        DataCollector.printData();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.frameCount % saveRate == 0)
-        {
-            DataCollector.positions[gameObjectCode] = this.gameObject.transform.position;
-            DataCollector.rotations[gameObjectCode] = this.gameObject.transform.rotation;
-            DataCollector.printData();
-        }
+        DataCollector.positions[gameObjectCode] = this.gameObject.transform.position;
+        DataCollector.rotations[gameObjectCode] = this.gameObject.transform.rotation;
     }
 }
